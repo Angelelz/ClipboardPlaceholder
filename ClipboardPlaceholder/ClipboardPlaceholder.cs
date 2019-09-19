7f904d2e2898ca6062a946cd74791776e24ed46c
Angelelz <angellopez20@gmail.com>
2019-09-18 22:31:00 -0400

    Initial Commit

---
+++ ClipboardPlaceholder/ClipboardPlaceholder.cs
@@ -0,0 +1,45 @@
+﻿using System.Text.RegularExpressions;
+using KeePass.Plugins;
+using KeePass.Util;
+using KeePass.Util.Spr;
+
+namespace ClipboardPlaceholder
+{
+    public sealed class ClipboardPlaceholderExt : Plugin
+    {
+        private const string mPlaceholder = "{Clipboard}";
+
+        private IPluginHost m_host = null;
+
+        public override string UpdateUrl
+        {
+            get { return "https://raw.githubusercontent.com/Angelelz/ClipboardPlaceholder/master/keepass.version"; }
+        }
+
+        public override bool Initialize(IPluginHost host)
+        {
+            m_host = host;
+            AutoType.FilterCompilePre += OnAutoTypeFilterCompilePre;
+            SprEngine.FilterPlaceholderHints.Add(mPlaceholder);
+            return true;
+        }
+
+        public override void Terminate()
+        {
+            SprEngine.FilterPlaceholderHints.Remove(mPlaceholder);
+            AutoType.FilterCompilePre -= OnAutoTypeFilterCompilePre;
+        }
+
+        private void OnAutoTypeFilterCompilePre(object sender, AutoTypeEventArgs autoTypeEventArgs)
+        {
+            Regex replacer = new Regex(Regex.Escape(mPlaceholder), RegexOptions.IgnoreCase);
+
+            autoTypeEventArgs.Sequence = replacer.Replace(autoTypeEventArgs.Sequence, match =>
+            {
+                var clip = ClipboardUtil.GetText();
+                return clip;
+            });
+        }
+
+    }
+}
